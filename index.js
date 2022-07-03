a517bbd2a43c0773c3bb3872f9aa9ba9d60d613d
Abhinav Kumar <ak800477@gmail.com>
2022-07-03 16:55:01 +0530

    ..

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 const express = require("express");
+
+// for intialializing socket
+const { createServer } = require("http");
+const { Server } = require("socket.io");
+
+const port = 5000;
+const app = express();
+
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
+  cors: { origin: ["http://localhost:3000"] },
+});
+
+//recieving the event
+io.on("connection", (socket) => {
+  console.log("client connected");
+
+  socket.on("sendmsg", (data) => {
+    console.log(data);
+    data.sent = false;
+    socket.broadcast.emit("recmsg", data);
+  });
+});
+
+const cors = require("cors");
+
+//middleware to convert client json data to javascript
+app.use(express.json());
+
+//cors is used for allowing front-end request
+app.use(cors({ origin: ["http://localhost:3000"] }));
+
+//route or endpoint
+app.get("/", (req, res) => {
+  res.send("response from express");
+});
+
+httpServer.listen(port, () => {
+  console.log("server started");
+});
